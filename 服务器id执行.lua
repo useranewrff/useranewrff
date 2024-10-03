375627085cf16960045c9ed6a2e17877ecb01a1c
useranewrff <141393397+useranewrff@users.noreply.github.com>
2024-10-03 21:22:20 +0800

    Update 服务器id执行.lua

--- 服务器id执行.lua
+++ 服务器id执行.lua
@@ -1,4 +1,4 @@
---力量传奇↓
-if game.Placeld == 1268927906 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/roblox/main/%E7%8C%8E%E6%88%B7%E5%BA%A7ui%E5%AF%BC%E7%AE%A1%E8%87%AA%E5%88%B6%E6%B7%B7%E6%B7%86.lua"))()
+--力量传奇
+if game.PlaceId == 1268927906 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/roblox/main/%E7%8C%8E%E6%88%B7%E5%BA%A7ui%E5%AF%BC%E7%AE%A1%E8%87%AA%E5%88%B6%E6%B7%B7%E6%B7%86.lua"))()
 end
