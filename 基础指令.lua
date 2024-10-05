9ebd2a81c083ae76631c57033866bb3fa43a1cf7
useranewrff <141393397+useranewrff@users.noreply.github.com>
2024-10-05 19:38:14 +0800

    Update 基础指令.lua

--- 基础指令.lua
+++ 基础指令.lua
@@ -1,38 +1,5 @@
-有infinite yield指令挂的看这里
-esp 看到玩家的血量距离名字(unesp)停止
-goto [玩家名字] 能够去到玩家后面
-fling 能够让你变成陀螺(unfling)停止
-creeper 能够让你变成苦力怕 但是要r6
-noclip 穿墙 (unnocilp)停止
-ID 能够查看玩家ID
-headsit [玩家名字] 能够让你坐在玩家头
-light 能够让你发光
-god 大部分游戏能够让你无敌
-bang 艹人
-Tpwalk [数字] 设定加速度
-speed [数字]
-jpower [数字]
-xray透视墙体 (unxray)停止
-re重置人物 但是重生之后位置不变
-nolegs 没有双腿
-noarms 没有手
-freecam 灵魂出窍
-full bright高亮度，玩恐怖游戏怕黑必备
-float 悬浮(unfloat)停止
-dance 跳舞(undance)停止
-sit 坐
-trip 正面倒地
-lay 背面倒地
-tptool 用了这个点击地面可以瞬移
-flyjump 长摁跳跃键可以飞
-nofog 删去烟雾（其实就是增加可见视野）
-chams 阉割版esp（不显示血量和名字）
-light 让你周围有光​
-carpet [玩家名字]让你被别人踩在脚下(uncarpet)停止-也可以不输入玩家名字↓
---上面那个仅限r6
-invisible 部分游戏隐身 有些游戏隐身会有bug
-tpua 创造一个黑洞把周围建筑吸走
-noanim 手上发光
-swim 不用在水里面 就可以游泳(unswim)停止
-f3x 没啥用，只有自己能看见 ​
-作者:不知主人的星怒
+game.StarterGui:SetCore("SendNotification", {
+    Title = "提示",
+    Text = "脚本已执行",
+    Duration = 5
+})
