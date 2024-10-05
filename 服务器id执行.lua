d46984d284f5bab1a83bfca5ccb7d1c958ba1417
useranewrff <141393397+useranewrff@users.noreply.github.com>
2024-10-05 11:11:33 +0800

    Add files via upload

--- 服务器id执行.lua
+++ 服务器id执行.lua
@@ -1,4 +1,52 @@
 --力量传奇
-if game.PlaceId == 1268927906 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/roblox/main/%E7%8C%8E%E6%88%B7%E5%BA%A7ui%E5%AF%BC%E7%AE%A1%E8%87%AA%E5%88%B6%E6%B7%B7%E6%B7%86.lua"))()
+if game.PlaceId == 3623096087 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/useranewrff/refs/heads/main/%E5%8A%9B%E9%87%8F%E4%BC%A0%E5%A5%87%E6%B7%B7%E6%B7%86.lua"))()
+--忍者传奇
+elseif game.PlaceId == 3956818381 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/useranewrff/refs/heads/main/%E5%BF%8D%E8%80%85%E4%BC%A0%E5%A5%87%E6%B7%B7%E6%B7%86.lua"))()
+--极速传奇
+elseif game.PlaceId == 3101667897 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/useranewrff/refs/heads/main/%E6%9E%81%E9%80%9F%E4%BC%A0%E5%A5%87%E6%B7%B7%E6%B7%86.lua"))()
+--起重模拟器
+elseif game.PlaceId == 3652625463 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/useranewrff/refs/heads/main/%E8%B5%B7%E9%87%8D%E6%A8%A1%E6%8B%9F%E5%99%A8.txt"))()
+--战争大亨
+elseif game.PlaceId == 4639625707 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/useranewrff/refs/heads/main/%E6%88%98%E4%BA%89%E5%A4%A7%E4%BA%A8.txt"))()
+--强壮传奇
+elseif game.PlaceId == 4399032158 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/useranewrff/refs/heads/main/%E5%BC%BA%E5%A3%AE%E4%BC%A0%E5%A5%87.txt"))()
+--吃掉世界
+elseif game.PlaceId == 16480898254 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/useranewrff/refs/heads/main/%E5%90%83%E6%8E%89%E4%B8%96%E7%95%8C.txt"))()
+--超级大力士模拟器
+elseif game.PlaceId == 6766156863 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/useranewrff/refs/heads/main/%E8%B6%85%E7%BA%A7%E5%A4%A7%E5%8A%9B%E5%A3%AB%E6%A8%A1%E6%8B%9F%E5%99%A8.txt"))()
+--短信模拟器
+elseif game.PlaceId == 2580982329 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/useranewrff/refs/heads/main/%E7%9F%AD%E4%BF%A1%E6%A8%A1%E6%8B%9F%E5%99%A8.txt"))()
+--造船寻宝
+elseif game.PlaceId == 537413528 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/useranewrff/refs/heads/main/%E9%80%A0%E8%88%B9%E5%AF%BB%E5%AE%9D.txt"))()
+--在51区生存
+elseif game.PlaceId == 155382109 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/useranewrff/refs/heads/main/%E5%9C%A851%E5%8C%BA%E7%94%9F%E5%AD%98.txt"))()
+--每秒跳跃加1
+elseif game.PlaceId == 11063612131 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/useranewrff/refs/heads/main/%E6%AF%8F%E7%A7%92%E8%B7%B3%E8%B7%83%2B1.txt"))()
+--地板是熔岩
+elseif game.PlaceId == 815405518 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/useranewrff/refs/heads/main/%E5%9C%B0%E6%9D%BF%E6%98%AF%E7%86%94%E5%B2%A9.txt"))()
+--雪人模拟器
+elseif game.PlaceId == 2533391464 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/useranewrff/refs/heads/main/%E9%9B%AA%E4%BA%BA%E6%A8%A1%E6%8B%9F%E5%99%A8.txt"))()
+--河北唐县
+elseif game.PlaceId == 461809391 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/useranewrff/refs/heads/main/%E6%B2%B3%E5%8C%97%E5%94%90%E5%8E%BF.txt"))()
+--餐厅大亨2
+elseif game.PlaceId == 3398014311 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/useranewrff/refs/heads/main/%E9%A4%90%E5%8E%85%E5%A4%A7%E4%BA%A8.txt"))()
+--鬼杀小队
+elseif game.PlaceId == 8069117419 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/useranewrff/refs/heads/main/%E9%AC%BC%E7%81%AD%E5%B0%8F%E9%98%9F.txt"))()
 end
