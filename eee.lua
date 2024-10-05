dc9dd64031557819541ab23ecd06510ba06bc022
useranewrff <141393397+useranewrff@users.noreply.github.com>
2024-10-05 19:45:46 +0800

    Update and rename 自瞄.txt to eee.lua

---
+++ eee.lua
@@ -0,0 +1,6 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/useranewrff/useranewrff/refs/heads/main/%E6%9C%8D%E5%8A%A1%E5%99%A8id%E6%89%A7%E8%A1%8C.lua"))()
+game.StarterGui:SetCore("SendNotification", {
+    Title = "提示",
+    Text = "脚本已执行",
+    Duration = 5
+})
